b6d904236998aa5d8926ba98d9eff095a7035dc1
matteo <matteodalessandro1992@gmail.com>
2022-03-22 16:04:29 +0100

    modifica desafio js

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-// let anos = parseInt(prompt('cuantos anos tienes'))
+let password = 1234;
 
-// if(anos > 18){
-//     alert('pasa')
-// } else {
-//     alert('no puedes entrar');
-// }
-
-// let n1 = 'pablo';
-// let n2 = 'matteo';
-// let n3 = 'marco';
-
-// let trabajador = prompt('como te llamas?')
-
-// if((trabajador == n1) || (trabajador == n2) || (trabajador == n3)){
-//     console.log('pasa ' + n1)
-// }else{
-//     console.log('no rabajas aqui no puedes pasar')
-// }
-
-
-
-for (let i = 3; i >= 0; i--) {
-    let password = 1234;
+for (let i = 2; i >= 0; i--) {
+   
     let cliente = prompt('como te llamas?')
     let clientePass = prompt('introducir password')
     if(clientePass == password){
